0efe38e6cada56d561b3ddef85d2405f46fbd1ba
Valerie Racine <vracine21@gmail.com>
2024-01-18 18:54:32 -0800

    refactor: createPlayer

--- rps.js
+++ rps.js
@@ -33,8 +33,8 @@ const readline = require("readline-sync");
 //Orchestration Engine: The engine is where the procedural program flow should be. 
 
 const RPSGame = {
-  human: createPlayer("human"),
-  computer: createPlayer("computer"),
+  human: createHuman(),
+  computer: createComputer(),
 
   displayWelcomeMessage() {
     console.log("Welcome to Rock, Paper, Scissors!");
@@ -87,40 +87,50 @@ const RPSGame = {
 
 RPSGame.play();
 
-
-function createPlayer(playerType) {
+function createPlayer() {
   return {
-  
-    playerType: playerType,
-    move: null,
-
-    choose() {
-      if (this.isHuman()) {
-        let choice;
-
-        while (true) {
-          console.log("Please choose rock, paper, or scissors.");
-          choice = readline.question();
-
-          if (["rock", "paper", "scissors"].includes(choice)) break;
-          console.log("Sorry, that's an invalid choice.");
-        }
-        
-        this.move = choice;
-      
-      } else {
-        const choices = ["rock", "paper", "scissors"];
-        let randomIndex = Math.floor(Math.random() * choices.length);
-        this.move = choices[randomIndex];
-      }
-    },
-
-    isHuman() {
-      return this.playerType === "human";
-    }
+    move: null, 
   };
 }
 
+function createComputer() {
+  let playerObject = createPlayer();
+
+  let computerObject = {
+    choose() {
+      const choices = ["rock", "paper", "scissors"];
+      let randomIndex = Math.floor(Math.random() * choices.length);
+      this.move = choices[randomIndex];
+    }
+  }
+
+  return Object.assign(playerObject, computerObject);
+}
+
+function createHuman() {
+  let playerObject = createPlayer();
+
+  let humanObject = {
+    choose() {
+      let choice;
+
+      while (true) {
+        console.log("Please choose rock, paper, or scissors.");
+        choice = readline.question();
+
+        if (["rock", "paper", "scissors"].includes(choice)) break;
+        console.log("Sorry, that's an invalid choice.");
+      }
+      
+      this.move = choice;
+    }
+  }
+  return Object.assign(playerObject, humanObject);
+}
+
+
+
+//extra features:
 function createMove() {
   return {
     // possible state: type of move (paper, rock, scissors)
